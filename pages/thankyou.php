f816e29d0a450ee44ee25b13c99fd7c0e3941e63
hoshiiionline <157491885+hoshiiionline@users.noreply.github.com>
2025-03-13 11:35:46 +0800

    Revert "Thank you page & Holiday counter"
    This reverts commit f4ebfdb97f5a3d437f3eb19b24103319f2876a7f.

--- pages/thankyou.php
+++ pages/thankyou.php
@@ -1,8 +1,6 @@
 <?php
 $title = "Thank You";
 include "../includes/header.php";
-
-$referenceNumber = $_SESSION['refNo'] ?? null;
 ?>
 
 <link rel="stylesheet" href="../css/returningCustomer.css">
@@ -12,8 +10,7 @@ $referenceNumber = $_SESSION['refNo'] ?? null;
 <div class="container" style="border-radius: 2em">
     <div><img src="../assets/logoflat.png" width="30%"></div>
     <h2>THANK YOU</h2>
-    <h3 style="padding-top: 1em">Your booking request has been received. A confirmation email with your booking details will be sent to your inbox shortly. If you have any questions, feel free to contact us.</h3>
-    <h3 style="font-weight: 500;">Reference Number: <?php echo $referenceNumber?></h3>
+    <h3 style="padding-top: 1em">Thank you! Your booking request has been received. A confirmation email with your booking details will be sent to your inbox shortly. If you have any questions, feel free to contact us.</h3>
     <h3 style="margin: 0; padding: 0; font-size: 1.5em;"><a class="back" href="https://banahawcircle.com/">Continue to Banahaw Circle</a></h3>
 </div>
 <?php include "../includes/footer.php"; ?>
