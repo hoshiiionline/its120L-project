3405fc12510b9c38ac594d438f3264097673f18c
TetheredAround <genekylemichaelf@gmail.com>
2025-03-18 17:28:30 +0800

    Updated API key and error handling for easier diagnosis.

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '851e00f49faa9a13d1dd5dd58f8f5cf247757b40',
+        'reference' => '3f2ce0604f384f89b8ee9438453636e34450174e',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '851e00f49faa9a13d1dd5dd58f8f5cf247757b40',
+            'reference' => '3f2ce0604f384f89b8ee9438453636e34450174e',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
