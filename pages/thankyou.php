0756fe8dd770d6cad73df3d5ca794510cd2fb4b1
hoshiiionline <157491885+hoshiiionline@users.noreply.github.com>
2025-03-19 10:05:36 +0800

    Update thankyou.php
    - Minor changes to thank you page

--- pages/thankyou.php
+++ pages/thankyou.php
@@ -16,6 +16,7 @@ session_destroy();
     <h2>THANK YOU</h2>
     <h3 style="padding-top: 1em">Your booking request has been received. A confirmation email with your booking details will be sent to your inbox shortly. If you have any questions, feel free to contact us.</h3>
     <h3 style="font-weight: 500;">Reference Number: <?php echo $referenceNumber?></h3>
+    <br>
     <h3 style="margin: 0; padding: 0; font-size: 1.5em;"><a class="back" href="https://banahawcircle.com/">Continue to Banahaw Circle</a></h3>
 </div>
 <?php include "../includes/footer.php"; ?>
